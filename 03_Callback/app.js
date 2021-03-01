bc00820a61f642b2600b909e01519f0155a480a5
rakesh.rankawat <rakesh.rankawat@DEL1-LHP-N70087>
2021-03-01 22:58:32 +0530

    Callback Functions

---
+++ 03_Callback/app.js
@@ -0,0 +1,51 @@
+// Callback Function - A function that passed into another function call, and called from that function is a callback function.
+
+const posts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
+];
+
+// Synchronous Way
+
+// function createPost(post) {
+//   setTimeout(function () {
+//     posts.push(post);
+//   }, 2000);
+// }
+
+// function getPosts() {
+//   setTimeout(function () {
+//     let output = "";
+//     posts.forEach(function (post) {
+//       output += `<li>${post.title}</li>`;
+//     });
+
+//     document.querySelector("#list").innerHTML = output;
+//   });
+// }
+
+// createPost({ title: "Post Three", body: "This is post three" });
+
+// getPosts();
+
+// Asynchronous Way
+
+function createPost(post, callback) {
+  setTimeout(function () {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+function getPosts() {
+  setTimeout(function () {
+    let output = "";
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`;
+    });
+
+    document.querySelector("#list").innerHTML = output;
+  });
+}
+
+createPost({ title: "Post Three", body: "This is post three" }, getPosts);
