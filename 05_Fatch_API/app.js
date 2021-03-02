7590d2b4275e311bb7f9d5f52566554a570e65fa
rakesh.rankawat <rakesh.rankawat@DEL1-LHP-N70087>
2021-03-02 23:33:27 +0530

    Arrow Functions

--- 05_Fatch_API/app.js
+++ 05_Fatch_API/app.js
@@ -5,51 +5,33 @@ document.querySelector("#button3").addEventListener("click", getExternal);
 // Text File
 function getText() {
   fetch("test.txt")
-    .then(function (res) {
-      return res.text();
-    })
-    .then(function (data) {
-      document.querySelector("#output").innerHTML = data;
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+    .then((res) => res.text())
+    .then((data) => (document.querySelector("#output").innerHTML = data))
+    .catch((err) => console.log(err));
 }
 
 // JSON File
 function getJson() {
   fetch("posts.json")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
+    .then((res) => res.json())
+    .then((data) => {
       let output = "";
-      data.forEach(function (post) {
-        output += `<li>${post.title}</li>`;
-      });
+      data.forEach((post) => (output += `<li>${post.title}</li>`));
 
       document.querySelector("#output").innerHTML = `<ul>${output}</u>`;
     })
-    .catch(function (err) {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 }
 
 // Rest API
 function getExternal() {
   fetch("https://api.github.com/users")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
+    .then((res) => res.json())
+    .then((data) => {
       let output = "";
-      data.forEach(function (user) {
-        output += `<li>${user.login}</li>`;
-      });
+      data.forEach((user) => (output += `<li>${user.login}</li>`));
 
       document.querySelector("#output").innerHTML = `<ul>${output}</u>`;
     })
-    .catch(function (err) {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 }
---
+++ 06_Arrow_Functions/app.js
@@ -0,0 +1,41 @@
+// Regular Function Expression
+// const sayHello = function () {
+//   console.log("Hello");
+// };
+
+// Arrow Function
+// const sayHello = () => {
+//   console.log("Hello");
+// };
+
+// One line function does not need braces
+// const sayHello = () => console.log("Hello");
+
+// One line returns
+// const sayHello = () => "Hello";
+
+// Returning Object Literal
+// const sayHello = () => ({ msg: "Hello" });
+
+// Parameters
+// const sayHello = (name) => console.log(name);
+// const sayHello = (firstName, lastName) =>
+//   console.log(`Hello ${firstName} ${lastName}`);
+
+// sayHello("Brad", "Traversy");
+
+const users = ["Nathan", "John", "William"];
+
+// const nameLengths = users.map(function (name) {
+//   return name.length;
+// });
+
+// Shorter
+// const nameLengths = users.map((name) => {
+//   return name.length;
+// });
+
+// Shortest
+// const nameLengths = users.map((name) => name.length);
+
+// console.log(nameLengths);
